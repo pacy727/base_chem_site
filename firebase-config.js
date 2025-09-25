48dde4b957f0f2dcecc724000dc9aff2c8052a2c
pacy <65156861+pacy727@users.noreply.github.com>
2025-09-25 20:24:29 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "ここに新しいAPIキーを入れる",
+    authDomain: "molmaster.firebaseapp.com",
+    projectId: "molmaster",
+    storageBucket: "molmaster.firebasestorage.app",
+    messagingSenderId: "8887697690",
+    appId: "1:8887697690:web:c65855cbbc8c994ae5eca3",
+    measurementId: "G-R2EQPP7822"
+};
+
+export default firebaseConfig;
